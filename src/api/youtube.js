44392399ee43723cc8b844cc132f7f61e7a68950
Surawich Sittisarn <surawich.sit@gmail.com>
2021-05-12 14:17:34 +0700

    add env

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,5 @@
+require("dotenv").config();
 import axios from "axios";
-
-const KEY = "AIzaSyBQX5dUFQ9QpKgoR42dKe166giDRBHUeFU";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
@@ -8,6 +7,6 @@ export default axios.create({
     part: "snippet",
     type: "video",
     maxResults: 5,
-    key: KEY,
+    key: YOUTUBE_API_KEY,
   },
 });
